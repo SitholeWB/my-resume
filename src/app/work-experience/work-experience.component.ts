818dac45dfb8327ae78f2d44377a0d4ebb1563fd
Welcome Sithole <sitholewb@gmail.com>
2020-08-10 15:45:37 +0200

    add more details on experience

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -9,9 +9,16 @@ export class WorkExperienceComponent implements OnInit {
   lexisnexisURL: string = "https://www.lexisnexis.co.za";
   derivcoURL: string = "https://www.derivco.co.za";
   astelURL: string = "http://astel.info";
+  refcheckURL: string = "https://www.refcheck.co.za";
+  expandCompany: string = "";
   constructor() { }
 
+
   ngOnInit(): void {
+  }
+
+  public toggleCompanyDetails(company: string) {
+    this.expandCompany = company;
   }
 
 }
